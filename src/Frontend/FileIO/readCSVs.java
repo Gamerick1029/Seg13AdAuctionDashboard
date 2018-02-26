6514e1b89b76ece587c361f5a073ea56fbf5f9ba
Jacob <jb21g14@soton.ac.uk>
2018-02-26 11:00:49 +0000

    Beginning of a redo on CSV parsing. Adding a test sources folder to not be included in release compiles.

--- src/Frontend/FileIO/readCSVs.java
+++ src/Frontend/FileIO/readCSVs.java
@@ -5,22 +5,18 @@ import java.io.FileNotFoundException;
 
 public class readCSVs {
 
-    public static void readClicks(String fileName) throws FileNotFoundException {
-        File file = stringToFile(fileName);
+    public static void readClicks(File file) {
+
     }
 
-    public static void readImpressions(String fileName) throws FileNotFoundException {
-        File file = stringToFile(fileName);
+    public static void readImpressions(String fileName) {
+
     }
 
-    public static void readServerLogs(String fileName) throws FileNotFoundException {
-        File file = stringToFile(fileName);
+    public static void readServerLogs(String fileName) {
+
     }
 
-    private static File stringToFile(String fileName) throws FileNotFoundException {
-        File file = new File(fileName);
-        if (!file.isFile()){ throw new FileNotFoundException("Couldn't locate file with name: "+ fileName); }
-        return file;
-    }
+
 
 }
