4d7e5451504371d81371f9230ea9ccd5a04ba3fb
Benedict Cairns <bc5g15@soton.ac.uk>
2018-02-26 12:46:10 +0000

    Added a simple unit test for the stub class

---
+++ Tests/Backend/Model/Interfaces/Stubs/ClickStubTest.java
@@ -0,0 +1,29 @@
+package Backend.Model.Interfaces.Stubs;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Benedict on 26/02/2018.
+ * A thoroughly redundant test that my stub class works
+ */
+class ClickStubTest
+{
+    @Test
+    void getDate()
+    {
+        assert(true);
+    }
+
+    @Test
+    void getID()
+    {
+    }
+
+    @Test
+    void getCost()
+    {
+    }
+
+}
