8a386e6d2fe522234c9f21d9e226be4d8fb9ddc5
Benedict Cairns <bc5g15@soton.ac.uk>
2018-03-23 11:11:18 +0000

    Added button to open the view screen using stub

--- src/Frontend/sample/WelcomeController.java
+++ src/Frontend/sample/WelcomeController.java
@@ -1,6 +1,9 @@
 package Frontend.sample;
 
 
+import Backend.Model.CampaignModel;
+import Backend.Model.Interfaces.DataModel;
+import Backend.Model.Stubs.DataModelStub;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
@@ -16,5 +19,18 @@ public class WelcomeController implements ScreenInterface {
     @FXML
     private void goToLoadDataScreen(ActionEvent event){
         myController.setScreen(Main.loadDataScreenID);
+    }
+
+    /*
+    Load a debug view from the stub data
+     */
+    @FXML
+    private void goToStubView(ActionEvent e)
+    {
+        DataModel dm = new DataModelStub();
+        myController.setDataModel(dm);
+        myController.getDataFieldPopulator().populateFields();
+        myController.getCampaignDataPopulator().populateGraph();
+        myController.setScreen(Main.campaignScreenID);
     }
 }
