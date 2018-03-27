ea7687d65390b60e1e18c84fbdd237c3f69f4de6
Jacob <jb21g14@soton.ac.uk>
2018-03-27 21:34:37 +0200

    Database can now be populated from the 3 input files. DBHelper class added to act as middleman for acquiring dbconnections and retrieving some semi-trivial pieces of data

---
+++ src/Backend/DBHelper.java
@@ -0,0 +1,55 @@
+package Backend;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBHelper {
+
+    public static final String dbName = "AdAuctionData";
+    public static final String defaultHost = "hobbithouse.me";
+    public static final String defaultPort = "3306";
+
+    public static final String defaultConnectionUrl = "jdbc:mariadb://" + defaultHost + ":" + defaultPort + "/" + dbName;
+
+    public static final String userTableSuffix = "_users";
+    public static final String impressionTableSuffix = "_impressions";
+    public static final String clickTableSuffix = "_clicks";
+    public static final String serverLogTableSuffix = "_serverLogs";
+
+    private Connection connection;
+
+    /**
+     * Middleman for acquiring a connection to the database.
+     * @param user
+     * @param pass
+     * @throws SQLException
+     */
+    public DBHelper(String user, String pass) throws SQLException {
+        connection = DriverManager.getConnection(defaultConnectionUrl, user, pass);
+    }
+
+    /**
+     * Returns a connection to the default database
+     * @return The connection to the database
+     * @throws SQLException If a connection cannot be made
+     */
+    public Connection getDefaultConnection() throws SQLException {
+        return connection;
+    }
+
+    /**
+     * Returns the list of campaigns currently in the database
+     * @return
+     * @throws SQLException
+     */
+    public ArrayList<String> getCampaigns() throws SQLException {
+        ResultSet rs = connection.createStatement().executeQuery("SELECT * FROM campaignNames;");
+        ArrayList<String> campaigns = new ArrayList<>();
+        while(rs.next()){
+            campaigns.add(rs.getString(1));
+        }
+        return campaigns;
+    }
+
+}
--- src/Backend/FileIO/ReadCSVsToDB.java
+++ src/Backend/FileIO/ReadCSVsToDB.java
@@ -1,150 +1,175 @@
 package Backend.FileIO;
 
-import Backend.MagicDB;
+import Backend.DBHelper;
 import com.opencsv.CSVReader;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Iterator;
 
 public class ReadCSVsToDB {
 
-    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss"); //DateTime format as specified in the CSV files
+    private static final SimpleDateFormat inDate = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss"); //DateTime format as specified in the CSV files
 
     private static final String DefaultDataBaseIP = "hobbithouse.me";
 
-    private int clickProgress = 0;
-    private int impressionProgress = 0;
-    private int serverProgress = 0;
+    private static int clickProgress = 0;
+    public static float impressionProgress = 0;
+    private static int serverProgress = 0;
 
-    private String currentIP;
-    private String campaignName;
-    private File clickLog;
-    private File impressionLog;
-    private File serverLog;
-    private String user;
-    private String pass;
+    private static String campaignName;
+    private static File clickLog;
+    private static File impressionLog;
+    private static File serverLog;
 
-    private Connection dbConnection;
+    private static Connection dbConnection;
 
     /**
-     * Default constructor for the class. Uses the hardcoded domain to connect to the MariaDB instance.
-     * @param campaignName
-     * @param clickLog
-     * @param impressionLog
-     * @param serverLog
+     * Used to create a new campaign in the database by supplying 3 data files and a connection to the desired database
+     * @param connection The database connection to pipe data into
+     * @param campaignName The name of the campaign to be created
+     * @param clickLog The click log file
+     * @param impressionLog The impression log file
+     * @param serverLog The server log file
+     * @throws SQLException If any sql statement is malformed at any stage of the process or there are issues with the connection this will be thrown
+     * @throws IOException If there is an issue accessing the files
      */
-    public ReadCSVsToDB(String campaignName, File clickLog, File impressionLog, File serverLog, String user, String pass) throws SQLException, IOException {
-        this(campaignName, clickLog, impressionLog, serverLog, user, pass, DefaultDataBaseIP);
+    public static void makeCampaign(Connection connection, String campaignName, File clickLog, File impressionLog, File serverLog) throws SQLException, IOException {
+        dbConnection = connection;
+        ReadCSVsToDB.campaignName = campaignName;
+        ReadCSVsToDB.clickLog = clickLog;
+        ReadCSVsToDB.impressionLog = impressionLog;
+        ReadCSVsToDB.serverLog = serverLog;
+
+        long start = System.nanoTime();
+        dbConnection.setAutoCommit(false);
+        createTables();
+        loadFilesToDB();
+        dbConnection.setAutoCommit(true);
+        System.out.println((System.nanoTime() - start)/1000000000.0);
     }
 
-    /**
-     * Allows for specifying a custom domain/ip for the MariaDB instance
-     * @param campaignName
-     * @param clickLog
-     * @param impressionLog
-     * @param serverLog
-     * @param dataBaseIP
-     */
-    public ReadCSVsToDB(String campaignName, File clickLog, File impressionLog, File serverLog, String user, String pass, String dataBaseIP) throws SQLException, IOException {
-        this.campaignName = campaignName;
-        this.clickLog = clickLog;
-        this.impressionLog = impressionLog;
-        this.serverLog = serverLog;
+    private static void createTables() throws IOException, SQLException {
+        String fileContents = FileHelpers.readFileToString("sqlScripts/sqlCreateTables.sql", Charset.defaultCharset());
+        fileContents = fileContents.replaceAll("\\[CAMPAIGN]", campaignName);
+        File temp = File.createTempFile("sqlRegexed", ".sql");
+        temp.deleteOnExit();
 
-        currentIP = dataBaseIP;
+        //Adds the campaign Name to the table of campaigns
+        //TODO: Currently does not discriminate on existing campaigns and will overwrite them. Need to decide what to do about that
+        dbConnection.createStatement().execute("INSERT INTO campaignNames VALUES ('" + campaignName +"') ON DUPLICATE KEY UPDATE name=name;");
 
-        String sql = "jdbc:mariadb://" + currentIP + ":" + MagicDB.defaultPort + "/" + MagicDB.dbName;
-        dbConnection = DriverManager.getConnection(sql, user, pass);
+        FileOutputStream fis = new FileOutputStream(temp);
+        fis.write(fileContents.getBytes());
+        fis.close();
 
         ScriptRunner sr = new ScriptRunner(dbConnection, true, true);
         try {
-            sr.runScript(new BufferedReader(new FileReader("sqlScripts/sqlCreateTables.sql")));
+            sr.runScript(new BufferedReader(new FileReader(temp)));
         } catch (IOException e) {
-            throw new IOException("Could not find sql Scripts File at sqlScripts/sqlCreateTables.sql");
+            throw new IOException("Could not access sql Scripts File at sqlScripts/sqlCreateTables.sql");
         }
-
-        loadFilesToDB();
     }
 
-    private void loadFilesToDB(){
+    private static void loadFilesToDB(){
+
         try {
-            loadClickLog();
+            loadImpressionLog();
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (ParseException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        loadImpressionLog();
-        loadServerLog();
+
+        try {
+            loadClickLog();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            loadServerLog();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void loadClickLog() throws SQLException, FileNotFoundException, ParseException {
-        int lineNum = FileHelpers.countLines(clickLog);
-        int percentPerLine = 100/lineNum;
+    private static void loadImpressionLog() throws SQLException, ParseException, IOException {
+        int lineNum = FileHelpers.countLines(impressionLog);
+        System.out.println(lineNum);
+        float percentPerLine = 100/lineNum;
 
         Statement stmt = dbConnection.createStatement();
-        String clickTableName = campaignName + MagicDB.clickTableSuffix;
-        String userTableName = campaignName + MagicDB.userTableSuffix;
+        String impressionTableName = campaignName + DBHelper.impressionTableSuffix;
+        String userTableName = campaignName + DBHelper.userTableSuffix;
 
-        CSVReader reader = new CSVReader(new FileReader(clickLog));
+        CSVReader reader = new CSVReader(new FileReader(impressionLog));
 
         Iterator<String[]> lines = reader.iterator();
 
         //Skips header line
         lines.next();
 
+        File users = File.createTempFile("users", ".csv");
+        File impressions = File.createTempFile("impressions", ".csv");
+        users.deleteOnExit();
+        impressions.deleteOnExit();
+
+        BufferedWriter fwUsers = new BufferedWriter(new FileWriter(users));
+        BufferedWriter fwImpressions = new BufferedWriter(new FileWriter(impressions));
+
+        long start = System.nanoTime();
         while (lines.hasNext()){
+
             String[] tokens = lines.next();
-            Date date = sdf.parse(tokens[0]);
-            String id = tokens[1];//TODO: See if compatible with UUID
-            String ageRange = tokens[2];//TODO: Possibly to be changed in to Enum
-            String gender = tokens[3];
+            String date = tokens[0];
+            String id = tokens[1];
+            String gender = tokens[2];
+            String ageRange = tokens[3];
             String income = tokens[4];
             String context = tokens[5];
             String impressionCost = tokens[6];
 
-            stmt.execute("INSERT INTO " + userTableName + "VALUES (" +
-            id + "," +
-            ageRange + "," +
-            gender + "," +
-            income + "," +
-            context +
-            "ON DUPLICATE KEY UPDATE id=id" //This line to not reinsert existing fields in to the specified table, whilst still erroring out otherwise
-            );
+            fwUsers.write(id + "," + gender + "," + ageRange + "," + income + "\n");
+            fwImpressions.write(date + "," + id + "," + context + "," + impressionCost + "\n");
 
-            stmt.execute("INSERT INTO " + clickTableName + "VALUES (" +
-            date.toString() + "," +
-            id + "," +
-            impressionCost
-            );
+//            System.out.println(id + "," + gender + "," + ageRange + "," + income + "\n");
+//            System.out.println(date + "," + id + "," + context + "," + impressionCost + "\n");
 
-            clickProgress += percentPerLine;
         }
+
+//        System.out.println((System.nanoTime() - start)/1000000000.0);
+
+        long startLoadDataINFILE = System.nanoTime();
+        stmt.execute("LOAD DATA LOCAL INFILE '" + users.getAbsolutePath() + "' INTO TABLE " + userTableName
+                        + " FIELDS TERMINATED BY ',';");
+        stmt.execute("LOAD DATA LOCAL INFILE '" + impressions.getAbsolutePath() + "' INTO TABLE " + impressionTableName
+                + " FIELDS TERMINATED BY ',';");
+//        System.out.println((System.nanoTime() - startLoadDataINFILE)/1000000000.0);
+
 
         clickProgress = 100;
     }
 
-    private void loadImpressionLog() {
-
+    private static void loadClickLog() throws SQLException{
+        String clickTableName = campaignName + DBHelper.clickTableSuffix;
+        Statement stmt = dbConnection.createStatement();
+        stmt.execute("LOAD DATA LOCAL INFILE '" + clickLog.getAbsolutePath() + "' INTO TABLE " + clickTableName
+                + " FIELDS TERMINATED BY ',';");
     }
 
-    private void loadServerLog() {
-
-    }
-
-    /**
-     * Used to get the connection to the server once the parent Object has been initialised
-     * @return
-     * @throws SQLException
-     */
-    public Connection getDBConnection() {
-        return dbConnection;
+    private static void loadServerLog() throws SQLException {
+        String serverTableName = campaignName + DBHelper.serverLogTableSuffix;
+        Statement stmt = dbConnection.createStatement();
+        stmt.execute("LOAD DATA LOCAL INFILE '" + serverLog.getAbsolutePath() + "' INTO TABLE " + serverTableName
+                + " FIELDS TERMINATED BY ',';");
     }
 
 }
